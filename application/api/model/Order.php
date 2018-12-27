11720c12228bc1f56f7a4c97cdac721db39ecadc
Gao Xiaonao <shelizi2011@163.com>
2018-12-27 17:55:30 +0800

    edit api/model/Order.php

--- application/api/model/Order.php
+++ application/api/model/Order.php
@@ -11,9 +11,22 @@ use app\lib\exception\BannerMissException;
 use think\Exception;
 
 class Order extends BaseModel {
+    /**
+     * @param $order
+     * @return $this
+     */
     static public function add($order){
         $result = self::create($order);
         return $result;
     }
 
+    /**
+     * @param $id
+     * @return null|static
+     * @throws \think\exception\DbException
+     */
+    static public function getOrderById($id){
+        $result = self::get($id);
+        return $result;
+    }
 }
